972c7ed532f5be9f7f1000caac002466557df163
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-07 18:14:59 +0700

    make transfer feature

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,4 +1,6 @@
-const transactionModels = require("../models/products");
+const transactionModels = require("../models/transactions");
+const userModels = require("../models/users")
+const { v4: uuid } = require("uuid");
 
 const history = async (req, res, next) => {
   try {
@@ -20,6 +22,55 @@ const history = async (req, res, next) => {
   }
 };
 
+const transaction = async (req, res, next) => {
+  try {
+    const { idUserTransfer, idUserTopup, amount, description } = req.body
+    const status = "success"
+
+    if (!idUserTransfer) return res.status(400).send({ message: "idUserTransfer cannot be null" })
+    if (!idUserTopup) return res.status(400).send({ message: "idUserTransfer cannot be null" })
+    if (!amount) return res.status(400).send({ message: "amount cannot be null" })
+    if (!description) return res.status(400).send({ message: "description cannot be null" })
+
+    const data = {
+      id: uuid().split("-").join(""),
+      idUserTransfer,
+      idUserTopup,
+      amount,
+      description,
+      status
+    }
+
+    const userTransfer = await userModels.getUsersById(idUserTransfer)
+    const userTopup = await userModels.getUsersById(idUserTopup)
+
+    const response = await transactionModels.transaction(data)
+
+    let amountUserTransfer = userTransfer.amount
+    let amountuserTopup = userTopup.amount
+
+    amountUserTransfer = amountUserTransfer - amount
+    amountuserTopup = amountuserTopup + amount
+
+    userTransfer.amount = amountUserTransfer
+    userTopup.amount = amountuserTopup
+
+    await userModels.updateUser(idUserTransfer, userTransfer)
+    await userModels.updateUser(idUserTopup, userTopup)
+
+    response.info = "Transfer Success"
+    data.message = response.info
+    data.status = true
+    data.balanceLeft = amountUserTransfer
+    res.json({
+      data
+    });
+  } catch (error) {
+    next(new Error(error.message));
+  }
+}
+
 module.exports = {
-  history
+  history,
+  transaction
 };
--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const transactionRoutes = require('../controllers/trasactions');
+const transactionRoutes = require('../controllers/transactions');
 
 
 router
+  .post('/', transactionRoutes.transaction)
   .get('/history', transactionRoutes.history)
 
 module.exports = router
