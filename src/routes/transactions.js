1f31fa776bf93ddade4a488800eba743dec60918
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-07 19:22:31 +0700

    Revert "Revert "Revert "make transfer feature"""
    This reverts commit 18609f4f9a666e59039aa40d64200c462108f902.

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const transactionRoutes = require('../controllers/transactions');
+const transactionRoutes = require('../controllers/trasactions');
 
 
 router
-  .post('/', transactionRoutes.transaction)
   .get('/history', transactionRoutes.history)
 
 module.exports = router
