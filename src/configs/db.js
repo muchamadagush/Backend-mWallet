83f0c294c2568e9159447c93d4eba8aba43e6e95
CandaMuammal <scmuammal@gmail.com>
2021-08-08 09:48:56 +0700

    getallusser

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,11 +1,10 @@
-const mysql = require('mysql2');
-require('dotenv').config;
+const mysql = require('mysql2')
 
-const conn = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-});
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD
+})
 
-module.exports = conn;
+module.exports = connection
