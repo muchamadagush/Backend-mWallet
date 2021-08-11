f7294ea06cabbc795ad797d99e1b9f51ee6fe8b4
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-11 13:28:09 +0700

    setting db mysql

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2')
 
-const connection = mysql.createPools({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
